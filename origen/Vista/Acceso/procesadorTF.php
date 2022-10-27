d40bd5dda767817be38c26b6122bbc54e3e2c118
vgto1986 <113908692+vgto1986@users.noreply.github.com>
2022-10-27 14:34:26 -0300

    Update and rename src/View/Login/procesadorTF.php to origen/Vista/Acceso/procesadorTF.php

---
+++ origen/Vista/Acceso/procesadorTF.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<title> OPCIONES DE TRABAJO  </title>
+	<!-- CSS -->
+		<link rel="stylesheet" href="css/style.css"
+	</head>
+	<body>
+				
+		<!-- CONTENIDO DEL FORMULARIO -->
+
+		<form action="procesadorTF.php" method="POST">
+		<section class="form-eleccion">
+		<h1 id="titulo"> OPCIONES DE TRABAJO </h1>
+			
+		 <!-- BOTON EL REGISTRO DE AVERIAS -->
+		<input id="btn" class="buttons" type="submit" value="Registro de averias">
+		
+		<!-- BOTON PARA EL LAVADO -->
+		<input id="btn2" class="buttons" type="submit" value="Lavado">
+		</form>
+		
+	
+</body>
--- src/View/Login/procesadorTF.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$dni = $_POST ['dni'];
-	echo "Hola ",$dni,", por favor cargue los datos correspondientes"; 
-
-?>
